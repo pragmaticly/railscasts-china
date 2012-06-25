d8b01677c7f0743c664a23ae75196eb155ec219c
Terry Tai <poshboytl@gmail.com>
2012-06-25 17:02:14 +0800

    Change the order make me feel better. :)

--- app/controllers/episodes_controller.rb
+++ app/controllers/episodes_controller.rb
@@ -4,6 +4,6 @@ class EpisodesController < ApplicationController
   end
 
   def index
-    @episodes = Episode.page(params[:page]).by_tag(params[:tag_name])
+    @episodes = Episode.by_tag(params[:tag_name]).page(params[:page])
   end
 end
