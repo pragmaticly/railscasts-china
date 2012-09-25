7c0342702117d7596841949968b4a101081d9845
Terry Tai <poshboytl@gmail.com>
2012-09-25 11:49:10 +0800

    Add the SSL hotfix to repository temporarily. Remove it when we find a better solution.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,10 @@
+module OpenSSL
+  module SSL
+    remove_const :VERIFY_PEER
+  end
+end
+OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
+
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :github, ENV["RAILSCASTS_GITHUB_CLIENT_ID"], ENV["RAILSCASTS_GITHUB_CLIENT_SECRET"]
 end
