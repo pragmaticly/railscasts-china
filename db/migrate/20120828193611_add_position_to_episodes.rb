3f1c38faf7ba9933089ed3d61974bd23336763cd
Terry Tai <poshboytl@gmail.com>
2013-01-04 00:58:19 +0800

    Make the position filed default to 0 which resolved the issue for creating episode in new environment.

--- db/migrate/20120828193611_add_position_to_episodes.rb
+++ db/migrate/20120828193611_add_position_to_episodes.rb
@@ -1,5 +1,5 @@
 class AddPositionToEpisodes < ActiveRecord::Migration
   def change
-    add_column :episodes, :position, :integer
+    add_column :episodes, :position, :integer, default: 0
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -15,15 +15,14 @@ ActiveRecord::Schema.define(:version => 20121016072523) do
 
   create_table "comments", :force => true do |t|
     t.integer  "episode_id"
+    t.integer  "user_id"
     t.text     "content"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "user_id"
     t.string   "ancestry"
   end
 
   add_index "comments", ["ancestry"], :name => "index_comments_on_ancestry"
-  add_index "comments", ["episode_id"], :name => "index_comments_on_episode_id"
 
   create_table "episodes", :force => true do |t|
     t.string   "name"
@@ -31,28 +30,25 @@ ActiveRecord::Schema.define(:version => 20121016072523) do
     t.text     "description"
     t.text     "notes"
     t.datetime "published_at"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.integer  "position",       :default => 0
-    t.integer  "comments_count", :default => 0,     :null => false
     t.integer  "seconds"
     t.text     "file_sizes"
-    t.boolean  "publish",        :default => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "user_id"
+    t.integer  "comments_count", :default => 0
     t.string   "still"
-    t.integer  "user_id",        :default => 1
+    t.boolean  "publish"
+    t.integer  "position",       :default => 0
     t.string   "video_url"
     t.string   "download_url"
   end
 
   create_table "taggings", :force => true do |t|
-    t.integer  "episode_id"
     t.integer  "tag_id"
+    t.integer  "episode_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
-
-  add_index "taggings", ["episode_id"], :name => "index_taggings_on_episode_id"
-  add_index "taggings", ["tag_id"], :name => "index_taggings_on_tag_id"
 
   create_table "tags", :force => true do |t|
     t.string   "name"
@@ -61,25 +57,13 @@ ActiveRecord::Schema.define(:version => 20121016072523) do
   end
 
   create_table "users", :force => true do |t|
-    t.string   "token"
+    t.string   "provider"
+    t.string   "uid"
     t.string   "name"
     t.string   "email"
-    t.boolean  "admin"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "uid"
-    t.string   "provider"
+    t.boolean  "admin"
   end
-
-  create_table "versions", :force => true do |t|
-    t.string   "item_type",  :null => false
-    t.integer  "item_id",    :null => false
-    t.string   "event",      :null => false
-    t.string   "whodunnit"
-    t.text     "object"
-    t.datetime "created_at"
-  end
-
-  add_index "versions", ["item_type", "item_id"], :name => "index_versions_on_item_type_and_item_id"
 
 end
