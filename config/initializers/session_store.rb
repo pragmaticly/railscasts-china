b5cdb082a50049d68be2994a4250480dda239310
Eric Guo <eric.guo@sandisk.com>
2014-08-17 20:54:32 +0800

    after running rake rails:update for 4.1.4

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-RailscastsChina::Application.config.session_store :cookie_store, key: '_railscasts_china_session'
+Rails.application.config.session_store :cookie_store, key: '_railscasts_china_session'
