8c3c898924a57bcdfc6feb478988bd5c63bb214e
Eric Guo <eric.guo@sandisk.com>
2014-08-28 09:18:57 +0800

    Only allow vote if not exceed the max_allowed_vote_per_user

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -4,10 +4,17 @@ class VotesController < ApplicationController
 
   def create
     @episode = Episode.find(votes_params[:episode_id])
-    @episode.votes.create(votes_params.merge(user_id: current_user.id, election_id: @episode.election_id))
-    respond_to do |format|
-      format.html { redirect_to @episode, notice: 'Vote successful!' }
-      format.json
+    if current_user.votes.where(election_id: @episode.election_id).count < @episode.election.max_allowed_vote_per_user
+      @episode.votes.create(votes_params.merge(user_id: current_user.id, election_id: @episode.election_id))
+      respond_to do |format|
+        format.html { redirect_to @episode, notice: 'Vote successful!' }
+        format.json
+      end
+    else
+      respond_to do |format|
+        format.html { redirect_to @episode, alert: "Vote number per #{@episode.election.name} exceed, allow #{@episode.election.max_allowed_vote_per_user} only" }
+        format.json
+      end
     end
   end
 
