82a8f90641996bc1e3b9ad0f24f7b0dd961cc69d
Eric Guo <eric.guo@sandisk.com>
2014-08-29 20:50:21 +0800

    Rewrite to using set_episode to reduce duplicate.

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,9 +1,9 @@
 class VotesController < ApplicationController
   respond_to :html, :json
   before_action :authenticate_user!
+  before_action :set_episode, only: [:create, :destroy]
 
   def create
-    @episode = Episode.find(votes_params[:episode_id])
     if current_user.votes.where(election_id: @episode.election_id).count < @episode.election.max_allowed_vote_per_user
       @episode.votes.create(votes_params.merge(user_id: current_user.id, election_id: @episode.election_id))
       respond_to do |format|
@@ -19,13 +19,16 @@ class VotesController < ApplicationController
   end
 
   def destroy
-    @episode = Episode.find(votes_params[:episode_id])
     Vote.find(params[:id]).destroy
     redirect_to @episode, notice: 'Unvote successful'
   end
 
 
 private
+  def set_episode
+    @episode = Episode.find(votes_params[:episode_id])
+  end
+
   def votes_params
     params.require(:vote).permit(:episode_id)
   end
