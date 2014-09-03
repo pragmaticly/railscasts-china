0396d806408cac400358ff510d4a9d0b11c16106
Eric Guo <eric.guo@sandisk.com>
2014-09-03 09:42:33 +0800

    seems picture is also require here.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,5 +5,5 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-Rails.application.config.assets.precompile += %w( modernizr.js )
+Rails.application.config.assets.precompile += %w( modernizr.js jplayer.blue.monday.jpg jplayer.blue.monday.seeking.gif jplayer.blue.monday.video.play.png)
 
